eb00ffd0d7d61bbd69d757e417be1891f85602e7
fatih <alfatihrizqialfian@gmail.com>
2023-12-30 22:35:19 +0700

    restyling

--- app/page.jsx
+++ app/page.jsx
@@ -8,27 +8,26 @@ export default async function Home() {
 
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center py-16 text-black gap-8">
+      <main className="flex min-h-screen flex-col py-16 text-black gap-8 mx-16">
         <h1 className="font-bold text-2xl border-b border-[#ABABAB] py-4">
           My Simple Notes App
         </h1>
+        <Link href="/add-notes">
+          <Button colorScheme="blue">Add Note</Button>
+        </Link>
         {notes.length === 0 ? (
           <h1 className="font-bold text-xl">Tidak ada catatan</h1>
         ) : (
           <>
-            <div className='flex flex-row flex-wrap gap-8'>
+            <div className="flex flex-row flex-wrap gap-8">
               {notes.map((p) => (
-                <Link key={p.id} href={`/notes/${p.id}`} >
+                <Link key={p.id} href={`/notes/${p.id}`}>
                   <Notes id={p.id} title={p.title} />
                 </Link>
               ))}
             </div>
           </>
         )}
-
-        <Link href="/add-notes">
-          <Button colorScheme="blue">Add Note</Button>
-        </Link>
       </main>
     </>
   );
